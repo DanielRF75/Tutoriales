4624d6de5130369123b40c31b71f87758e6d8cba
DanielR75 <d.r.ferrer8@gmail.com>
2026-01-21 22:37:52 +0100

    intento 3

--- KioskLauncher/src/main/java/com/kiosk/Main.java
+++ KioskLauncher/src/main/java/com/kiosk/Main.java
@@ -1,9 +1,10 @@
+package com.kiosk;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
